df218546268bb6098ef88b907c92a48501fb7e6d
Mohnish Thallavajhula <arrowgunz@gmail.com>
2012-04-20 14:28:13 -0700

    fixed a typo

--- models/messages.js
+++ models/messages.js
@@ -1,5 +1,5 @@
 var database = require('../config/database').database;
 var mysql = database.mysql;
 var client = database.client;
-var DATABAE = 'twenty8';
+var DATABASE = 'twenty8';
 var TABLE = 'messages';
